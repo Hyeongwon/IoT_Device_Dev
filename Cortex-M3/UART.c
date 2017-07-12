7b50476bd1cbf0503b6191d2741c2760350aa701
Hyeongwon <bhw0506@gmail.com>
2017-07-12 09:22:09 +0900

    modify UART.c

--- Cortex-M3/UART.c
+++ Cortex-M3/UART.c
@@ -1,170 +1,300 @@
-/**
-  ******************************************************************************
-  * @file    IWDG_Reset/main.c 
-  * @author  MCD Application Team
-  * @version V1.2.0
-  * @date    11-April-2014
-  * @brief   Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
-  *
-  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  *        http://www.st.com/software_license_agreement_liberty_v2
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
-  ******************************************************************************
-  */
+#if 0
+#include "stm32f10x.h"
 
-/* Includes ------------------------------------------------------------------*/
-#include "main.h"
-
-/** @addtogroup IWDG_Reset
-  * @{
-  */ 
-
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
-__IO uint32_t TimingDelay = 0;
-__IO uint32_t LsiFreq = 40000;
-
-/* Private function prototypes -----------------------------------------------*/
-/* Private functions ---------------------------------------------------------*/
-
-/**
-  * @brief   Main program
-  * @param  None
-  * @retval None
-  */
-int main(void)
+int main()
 {
-  /*!< At this stage the microcontroller clock setting is already configured, 
-       this is done through SystemInit() function which is called from startup
-       file (startup_stm32f10x_md.s) before to branch to application main.
-       To reconfigure the default setting of SystemInit() function, refer to
-       system_stm32f10x.c file
-     */     
-       
-  /* Initialize LED2 and Key Button mounted on STM32NUCLEO board */       
-  STM_EVAL_LEDInit(LED2);
-  STM_EVAL_PBInit(BUTTON_USER, BUTTON_MODE_EXTI);
-
-  /* Setup SysTick Timer for 1 msec interrupts  */
-  if (SysTick_Config(SystemCoreClock / 1000))
-  { 
-    /* Capture error */ 
-    while (1);
-  }
+  RCC -> APB2ENR |= RCC_APB2Periph_GPIOA;
+  GPIOA -> CRL &=  ~(0xF << 12 | 0xF << 8);
+  GPIOA -> CRL |= (0x4 << 12 | 0xB << 8); // GPIOA.2 = TX / GPIOA.3 = RX
   
-  /* Check if the system has resumed from IWDG reset */
-  if (RCC_GetFlagStatus(RCC_FLAG_IWDGRST) != RESET)
-  {
-    /* IWDGRST flag set */  
-    /* Turn On LED2 */
-    STM_EVAL_LEDOn(LED2);
-    
-    /* Insert delay (3s) */
-    Delay(3000);
-    
-    /* Clear reset flags */
-    RCC_ClearFlag();
-  }
+  RCC->APB1ENR |= RCC_APB1Periph_USART2;
+  USART2 -> CR1 |= USART_Mode_Rx | USART_Mode_Tx;
+  USART2 -> BRR |= 0x00000341; // 8Mhz 클럭 사용시 9600 Bps;
   
-  /* IWDG timeout equal to 250 ms (the timeout may varies due to LSI frequency
-     dispersion) */
-  /* Enable write access to IWDG_PR and IWDG_RLR registers */
-  IWDG_WriteAccessCmd(IWDG_WriteAccess_Enable);
-
-  /* IWDG counter clock: LSI/32 */
-  IWDG_SetPrescaler(IWDG_Prescaler_32);
-
-  /* Set counter reload value to obtain 250ms IWDG TimeOut.
-     Counter Reload Value = 250ms * IWDG counter clock period
-                          = 250ms * (LSI/32)
-                          = 0.25s * (LsiFreq/32)
-                          = LsiFreq / (32 * 4)
-                          = LsiFreq / 128
-   */
-  IWDG_SetReload(LsiFreq/128);
-
-  /* Reload IWDG counter */
-  IWDG_ReloadCounter();
-
-  /* Enable IWDG (the LSI oscillator will be enabled by hardware) */
-  IWDG_Enable();
-
-  while (1)
-  {
-    /* Toggle LED2 */
-    STM_EVAL_LEDToggle(LED2);
-
-    /* Insert 220 ms delay */
-    Delay(220);
-
-    /* Reload IWDG counter */
-    IWDG_ReloadCounter();  
+  USART2 -> CR1 |= (1 << 13);
+  
+  while(1) {
+    
+    if((USART2 -> SR & (1 << 5)) != 0) {
+      
+      int data = USART2 -> DR;
+      while((USART2 -> SR & (1 << 7)) == 0) {}
+      USART2 -> DR = data;
+    }
   }
 }
 
-/**
-  * @brief  Inserts a delay time.
-  * @param  nTime: specifies the delay time length, in 1 ms.
-  * @retval None
-  */
-void Delay(__IO uint32_t nTime)
-{
-  TimingDelay = nTime;
+#endif
 
-  while(TimingDelay != 0);
+#if 0
+#include "stm32F10x.h"
+
+int main(void) {
+  
+  GPIO_InitTypeDef GPIO_InitStruct;
+  USART_InitTypeDef USART_InitStruct;
+  
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+  
+  GPIO_StructInit(&GPIO_InitStruct);
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_3;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  GPIO_Init(GPIOA, &GPIO_InitStruct);
+  
+  GPIO_StructInit(&GPIO_InitStruct);
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_2;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF_PP;
+  GPIO_Init(GPIOA, &GPIO_InitStruct);
+  
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+  
+  USART_StructInit(&USART_InitStruct);
+  USART_InitStruct.USART_BaudRate = 9600;
+  USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+  USART_InitStruct.USART_StopBits = USART_StopBits_1;
+  USART_InitStruct.USART_Parity = USART_Parity_No;
+  USART_InitStruct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+  USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_Init(USART2, &USART_InitStruct);
+  
+  USART_Cmd(USART2, ENABLE);
+  
+  USART_SendData(USART2, 'H');
 }
+#endif
 
-/**
-  * @brief  Decrements the TimingDelay variable.
-  * @param  None
-  * @retval None
-  */
-void TimingDelay_Decrement(void)
-{
-  if (TimingDelay != 0x00)
-  { 
-    TimingDelay--;
+#if 0
+//printf
+#include "stm32F10x.h"
+#include <stdio.h>
+/*
+int fputc(int ch, FILE *f) {
+  
+  static int last;
+  
+  if( (ch == (int) '\n') && (last != (int)'\r') ) {
+    
+    last = (int)'\r';
+    
+    while(USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
+    USART_SendData(USART2, last); // 1 (USART1)
   }
-}
-#ifdef  USE_FULL_ASSERT
+  else last = ch;
+  
+  while(USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
+  USART_SendData(USART2, ch);
+  return(ch);
+}*/
 
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t* file, uint32_t line)
-{ 
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+int main(void) {
+  
+  GPIO_InitTypeDef GPIO_InitStruct;
+  USART_InitTypeDef USART_InitStruct;
+  
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+  
+  GPIO_StructInit(&GPIO_InitStruct);
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_3;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  GPIO_Init(GPIOA, &GPIO_InitStruct);
+  
+  GPIO_StructInit(&GPIO_InitStruct);
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_2;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF_PP;
+  GPIO_Init(GPIOA, &GPIO_InitStruct);
+  
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+  
+  USART_StructInit(&USART_InitStruct);
+  USART_InitStruct.USART_BaudRate = 9600;
+  USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+  USART_InitStruct.USART_StopBits = USART_StopBits_1;
+  USART_InitStruct.USART_Parity = USART_Parity_No;
+  USART_InitStruct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+  USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_Init(USART2, &USART_InitStruct);
+  
+  USART_Cmd(USART2, ENABLE);
+  
+  USART_SendData(USART2, 'H');
 
-  /* Infinite loop */
-  while (1)
-  {
+  int16_t myInt = 0x1234;
+  float myFloat = (float)myInt;
+  
+  printf("myFloat %f\n\r", myFloat);
+  printf("myInt %x\n\r", myInt);
+  
+  while(1) {
   }
 }
 #endif
 
-/**
-  * @}
-  */
+#if 1
+//Two-Way-UART & Bluetooth module
+#include "stm32F10x.h"
+#include <stdio.h>
 
+int main(void) {
+  
+  GPIO_InitTypeDef GPIO_InitStruct;
+  USART_InitTypeDef USART_InitStruct;
+  
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+  
+  //UART1
+  GPIO_StructInit(&GPIO_InitStruct);
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_10;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  GPIO_Init(GPIOA, &GPIO_InitStruct);
+  
+  GPIO_StructInit(&GPIO_InitStruct);
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_9;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF_PP;
+  GPIO_Init(GPIOA, &GPIO_InitStruct);
+  
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+  
+  USART_StructInit(&USART_InitStruct);
+  USART_InitStruct.USART_BaudRate = 9600;
+  USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+  USART_InitStruct.USART_StopBits = USART_StopBits_1;
+  USART_InitStruct.USART_Parity = USART_Parity_No;
+  USART_InitStruct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+  USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_Init(USART1, &USART_InitStruct);
+  
+  USART_Cmd(USART1, ENABLE);
+  
+  //UART2
+  GPIO_StructInit(&GPIO_InitStruct);
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_3;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  GPIO_Init(GPIOA, &GPIO_InitStruct);
+  
+  GPIO_StructInit(&GPIO_InitStruct);
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_2;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF_PP;
+  GPIO_Init(GPIOA, &GPIO_InitStruct);
+  
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+  
+  USART_StructInit(&USART_InitStruct);
+  USART_InitStruct.USART_BaudRate = 9600;
+  USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+  USART_InitStruct.USART_StopBits = USART_StopBits_1;
+  USART_InitStruct.USART_Parity = USART_Parity_No;
+  USART_InitStruct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+  USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_Init(USART2, &USART_InitStruct);
+  
+  USART_Cmd(USART2, ENABLE);
+  
+   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+   
+  // GPIOC port1;
+  GPIO_StructInit(&GPIO_InitStruct);
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_1;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
+  GPIO_Init(GPIOC, &GPIO_InitStruct);
+  
+  // GPIOC port2;
+  GPIO_StructInit(&GPIO_InitStruct);
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  GPIO_Init(GPIOC, &GPIO_InitStruct);
+  
+  
+  while(1) {
+    
+    FlagStatus fstatus1 = USART_GetFlagStatus(USART1, USART_FLAG_RXNE);
+    FlagStatus fstatus2 = USART_GetFlagStatus(USART2, USART_FLAG_RXNE);
+    
+    unsigned int input = GPIOC->IDR;
+    
+    if( (input & 1) != 0) {
+      USART_SendData(USART2, '1');
+    }
+    
+    if(fstatus2 == SET) {
+      uint16_t rDate = USART_ReceiveData(USART2);
+      //USART_SendData(USART2, rDate2);
+      USART_SendData(USART2, rDate);
+      //printf("rDate  = %x\n", rDate);
+      //printf("GPIOC->IDR = %x\n", (input[0]));
+      
+      if(rDate == '1') {
+        
+        GPIO_SetBits(GPIOC, GPIO_Pin_1);
+      } else if(rDate == '0') {
+        
+        GPIO_ResetBits(GPIOC, GPIO_Pin_1);
+      }
+      
+      USART_ClearFlag(USART2, USART_FLAG_RXNE);
+    }
+    if(fstatus1 == SET) {
+      uint16_t rDate = USART_ReceiveData(USART1);
+      //USART_SendData(USART2, rDate2);
+      USART_SendData(USART2, rDate);
+      
+      USART_ClearFlag(USART1, USART_FLAG_RXNE);
+    }
+  }
+}
+#endif
 
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+#if 0
+#include "stm32F10x.h"
+
+RCC_ClocksTypeDef RCC_ClockFreq;
+
+int main(void) {
+  
+  // Meaning of ClockFreq
+  RCC_GetClocksFreq(&RCC_ClockFreq);
+  
+  //Set SysClock
+  
+  /* PLL configuration: = (HSI / 2) * 16 = 64 MHz */
+  RCC -> CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_PLLSRC |
+                                        RCC_CFGR_PLLMULL));
+  
+  RCC-> CFGR |= (uint32_t)(RCC_CFGR_PLLSRC_HSI_Div2 |
+                           RCC_CFGR_PLLMULL16);
+  
+  // Enable Prefetch Buffer
+  FLASH -> ACR |= FLASH_ACR_PRFTBE;
+  FLASH -> ACR &= (uint32_t)((uint32_t)~FLASH_ACR_LATENCY);
+  FLASH -> ACR |= (uint32_t)((uint32_t)FLASH_ACR_LATENCY_2);
+  
+  /*HCLK = SYSCLK */
+  RCC -> CFGR |= (uint32_t)RCC_CFGR_HPRE_DIV1;
+  
+  /*PCLK2 = HCLK */
+  RCC -> CFGR |= (uint32_t)RCC_CFGR_PPRE2_DIV1;
+  
+  /*PCLK1 = HCLK */
+  RCC -> CFGR |= (uint32_t)RCC_CFGR_PPRE1_DIV2;
+  
+  /* Enable PLL */
+  RCC -> CR |= RCC_CR_PLLON;
+  
+  /* Wait till PLL is ready */
+  while( (RCC -> CR & RCC_CR_PLLRDY) == 0) {}
+  
+  /* Select PLL as system clock source */
+  RCC -> CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_SW));
+  RCC -> CFGR |= (uint32_t)RCC_CFGR_SW_PLL;
+  
+  /*Wait till PLL is used as system clock source */
+  while( (RCC -> CFGR & (uint32_t)RCC_CFGR_SWS) != (uint32_t)0x08) {}
+  
+  RCC_GetClocksFreq(&RCC_ClockFreq);
+}
+#endif
