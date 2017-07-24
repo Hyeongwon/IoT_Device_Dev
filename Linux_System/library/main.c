700b48025702cf5993407a60ae7a45bd3c5d95d7
Hyeongwon <bhw0506@gmail.com>
2017-07-24 08:36:35 +0000

    Add Linux_System_ex_01

---
+++ Linux_System/library/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "mylib.h"
+
+int main() {
+
+    int a, b;
+    a = 100;
+    b = 50;
+
+    printf("sum = %d\n", sum(a, b));
+    printf("diff = %d\n", diff(a,b));
+    
+    return 0;
+}
